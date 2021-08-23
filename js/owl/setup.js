6502947bf16c6717bb460caa48ca590bb7f5eb16
Adm.DevOps.Léo Albergaria <63436406+leo-albergaria@users.noreply.github.com>
2021-08-23 11:01:56 -0300

    Add files via upload

---
+++ js/owl/setup.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop: false,
+    margin: 15,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        300: {
+            items: 4,
+        },
+        600: {
+            items: 8,
+        },
+    },
+});
